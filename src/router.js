3c5bb89bae9729a7beb9621701d89f95a1d495b2
Cayro Cerqueira <cayro.cerqueira@grancursosonline.com.br>
2022-03-26 16:40:53 -0300

    instala os pacotes iniciais

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import { BrowserRouter ,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import Home from "./pages/home";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
